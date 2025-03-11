7c896e28876a3a8481bbe74b5aee92de79b74d3c
myunsyeya <myunsyeya@ajou.ac.kr>
2025-03-11 10:48:40 +0900

    feat: 제스쳐 핸들러가 앱 초기에 전역적으로 동작 [SN-59]

--- App.tsx
+++ App.tsx
@@ -6,16 +6,24 @@
  */
 
 import React from 'react';
+import { StyleSheet } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import AppNavigation from './src/app/providers/navigation';
 import { ToastContainer } from './src/shared/ui/Toast';
 
 function App(): React.JSX.Element {
   return (
-    <>
+    <GestureHandlerRootView style={styles.container}>
       <AppNavigation />
       <ToastContainer />
-    </>
+    </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
