b63650af80abdf54f70d142d8dc371a0cd14cc86
LeeWxx <dnckd0903@naver.com>
2025-03-24 21:01:32 +0900

    feat: [iOS] feat: ModelManager 네이티브 모듈 추가 [SN-87]

---
+++ ios/Modules/ModelManager.m
@@ -0,0 +1,19 @@
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+
+@interface RCT_EXTERN_MODULE(ModelManager, RCTEventEmitter)
+
+RCT_EXTERN_METHOD(preloadModel:(NSString *)modelUrl
+                  modelType:(NSString *)modelType
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(getModelLoadStatus:(NSString *)modelType
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(getModelPath:(NSString *)modelType
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+
+@end
