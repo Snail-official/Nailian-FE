6aa5c1b1b021099e346cf841be2d0c1099115c54
myunsyeya <myunsyeya@ajou.ac.kr>
2025-04-16 11:54:15 +0900

    feat: 1:1, FAQ, 이용약관 추가 [SN-131]

--- src/pages/my_page/index.tsx
+++ src/pages/my_page/index.tsx
@@ -138,9 +138,9 @@ function MyPageScreen({ navigation }: MyPageProps) {
    */
   const handleMenuPress = async (menuType: string) => {
     const urls: Record<string, string> = {
-      '1:1 문의': 'https://example.com/inquiry',
-      FAQ: 'https://example.com/faq',
-      '약관 및 정책': 'https://example.com/terms',
+      '1:1 문의': 'https://www.notion.so/1-1-1d4a61f4f3718080af26de9177d78887',
+      FAQ: 'https://www.notion.so/FAQ-1d4a61f4f3718095891aec01cdbb82d5',
+      '약관 및 정책': 'https://www.notion.so/1d4a61f4f37180a89490fd3bde2b3a7b',
     };
     // URL이 있으면 웹페이지로 이동
     if (urls[menuType]) {
